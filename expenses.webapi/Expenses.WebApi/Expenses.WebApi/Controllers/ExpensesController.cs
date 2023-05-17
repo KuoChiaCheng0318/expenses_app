78b6e909f3d390a3c72933385d7a56448173ef0d
ChiaChengKuo <82024833+KuoChiaCheng0318@users.noreply.github.com>
2023-05-16 18:48:08 -0700

    Building web api with asp.net, have api return expenses

---
+++ expenses.webapi/Expenses.WebApi/Expenses.WebApi/Controllers/ExpensesController.cs
@@ -0,0 +1,23 @@
+﻿using Expenses.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expenses.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExpensesController : ControllerBase
+    {
+        private IExpensesServices _expensesServices;
+        public ExpensesController(IExpensesServices expensesServices)
+        {
+            _expensesServices = expensesServices;
+        }
+        [HttpGet]
+        public IActionResult GetExpenses()
+        {
+            return Ok(_expensesServices.GetExpenses());
+        }
+
+
+    }
+}
