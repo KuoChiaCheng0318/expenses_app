598cbfddac0483abfc7827b7847d283fd7c0c21c
ChiaChengKuo <82024833+KuoChiaCheng0318@users.noreply.github.com>
2023-05-20 17:36:54 -0700

    Deployed on Azure

--- expenses.webapi/Expenses.DB/AppDbContext.cs
+++ expenses.webapi/Expenses.DB/AppDbContext.cs
@@ -4,13 +4,13 @@ namespace Expenses.DB
 {
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
         public DbSet<Expense> Expenses { get; set; }
         public DbSet<User> Users { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(
-                @"Server=(localdb)\mssqllocaldb;Database=ExpensesDB;Integrated Security=True");
-        }
+        
     }
 }
